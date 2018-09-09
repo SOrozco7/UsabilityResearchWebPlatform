66053fba5b21a7b4c853e7e7106670eee3ac1f12
Syul968 <syul.pgn@gmail.com>
2018-09-09 16:00:36 -0500

    Added sequelize dependency and models

---
+++ backend/models/experiment.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Experiment = sequelize.define('Experiment', {
+    name: DataTypes.STRING,
+    startDateTime: DataTypes.DATE,
+    endDateTime: DataTypes.DATE
+  }, {});
+  Experiment.associate = function(models) {
+    Experiment.belongsTo(models.User, {
+    	foreignKey: 'userId'
+    });
+  };
+  return Experiment;
+};
