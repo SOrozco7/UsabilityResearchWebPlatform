7d845374a0f833d7a9bec4f01e92a53588d99865
ChavaO777 <salvador@orozco.in>
2018-09-10 21:23:59 -0500

    Fix the return type of the login function.

--- backend/service/UserService.js
+++ backend/service/UserService.js
@@ -6,11 +6,31 @@
  * This method logs the user in.
  *
  * body User User object that needs to be logged in.
- * no response value expected for this operation
+ * returns List
  **/
 exports.login = function(body) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    var examples = {};
+    examples['application/json'] = [ {
+  "firstName" : "firstName",
+  "lastName" : "lastName",
+  "password" : "password",
+  "id" : 0,
+  "email" : "email",
+  "username" : "username"
+}, {
+  "firstName" : "firstName",
+  "lastName" : "lastName",
+  "password" : "password",
+  "id" : 0,
+  "email" : "email",
+  "username" : "username"
+} ];
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
   });
 }
 
