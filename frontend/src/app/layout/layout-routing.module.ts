095df615de7ccb53684d37adb056448a3c13ae6c
guillegard <guillegard95@gmail.com>
2018-09-11 22:01:56 -0500

    frontend for experiment CRUD

--- frontend/src/app/layout/layout-routing.module.ts
+++ frontend/src/app/layout/layout-routing.module.ts
@@ -9,13 +9,13 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-            { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
+            { path: 'createExperiment', loadChildren: './charts/charts.module#ChartsModule' },
+            { path: 'experiments', loadChildren: './tables/tables.module#TablesModule' },
+            { path: 'editExperiment', loadChildren: './form/form.module#FormModule' },
             { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
             { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
             { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
+            { path: 'experiment', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
         ]
     }
 ];
