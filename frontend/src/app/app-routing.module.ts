bd847750ad2d5c79de5ed2aa9376c4aed1816cf0
ChavaO777 <salvador@orozco.in>
2018-10-03 22:37:13 -0500

    Add the generic route redirected to the 'experiments' route which could work as our 'Home' for now.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -47,11 +47,11 @@ const routes: Routes = [
     component: ExperimentDeleteComponent, 
     canActivate: [AuthGuard] 
   },
-  // // All other routes
-  // { 
-  //   path: '**', 
-  //   // redirectTo: 'experiments' 
-  // }
+  // All other routes
+  { 
+    path: '**', 
+    redirectTo: 'experiments' 
+  }
 ]
 
 @NgModule({
