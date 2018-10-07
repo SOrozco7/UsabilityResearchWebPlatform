0a52c8b02f13505113a77383af7a430003e469a4
ChavaO777 <salvador@orozco.in>
2018-10-07 14:10:05 -0500

    Add the spec file for the nav component.

--- frontend/src/app/components/nav/nav.component.spec.ts
+++ frontend/src/app/components/nav/nav.component.spec.ts
@@ -3,27 +3,27 @@ import { NavComponent } from './nav.component';
 import { Router } from '@angular/router';
 import { RouterStub } from  '../../router-stub';
 
-// describe('NavComponent', () => {
-//   // let component: NavComponent;
-//   // let fixture: ComponentFixture<NavComponent>;
+describe('NavComponent', () => {
+  let component: NavComponent;
+  let fixture: ComponentFixture<NavComponent>;
 
-//   // beforeEach(async(() => {
-//   //   TestBed.configureTestingModule({
-//   //     declarations: [ NavComponent ],
-//   //     providers: [
-//   //       {provide: Router, useClass: RouterStub}
-//   //     ]
-//   //   })
-//   //   .compileComponents();
-//   // }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavComponent ],
+      providers: [
+        {provide: Router, useClass: RouterStub}
+      ]
+    })
+    .compileComponents();
+  }));
 
-//   // beforeEach(() => {
-//   //   fixture = TestBed.createComponent(NavComponent);
-//   //   component = fixture.componentInstance;
-//   //   fixture.detectChanges();
-//   // });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   // it('should create', () => {
-//   //   expect(component).toBeTruthy();
-//   // });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
