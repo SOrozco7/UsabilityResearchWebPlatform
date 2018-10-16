ccfaa9cf73ee52a8f5553d591a3dd9db93974e66
Andrés Reynoso Mazoy <andres.reynoso.mazoy@gmail.com>
2018-10-16 12:44:57 -0500

    Added ignore for bodyparts model's attribute

--- frontend/src/app/models/body-part.ts
+++ frontend/src/app/models/body-part.ts
@@ -3,7 +3,7 @@ import { Experiment } from './experiment';
 export class BodyPart {
     constructor(
         public id?: number,
-        public bodyParts?: string[],
+        public BodyParts?: string[],  // tslint:disable-line:variable-name
         public experiment_id?: number
     ) { }
 }
