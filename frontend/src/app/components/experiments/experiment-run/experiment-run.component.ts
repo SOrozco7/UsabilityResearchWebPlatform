3c3f0083d44498b4478d0930955d5643857d7946
ChavaO777 <salvador@orozco.in>
2018-11-04 16:46:45 -0600

    Write the call to retrieve the experiment's questions and the first methods for handling the logic of the experiment execution.

--- frontend/src/app/components/experiments/experiment-run/experiment-run.component.ts
+++ frontend/src/app/components/experiments/experiment-run/experiment-run.component.ts
@@ -14,6 +14,9 @@ import { Question } from '../../../models/question';
 export class ExperimentRunComponent implements OnInit {
 
   experimentQuestions: Question[];
+  currQuestionIndex: number;
+  currentQuestion: Question;
+  questionCount: number;
   experimentId: number;
 
   constructor(
@@ -24,17 +27,76 @@ export class ExperimentRunComponent implements OnInit {
 
   ngOnInit() {
 
+    this.experimentQuestions = null;
     this.experimentId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
 
+    // Get the list of the experiment's questions by retrieving the experiment
     this.crud.retrieve(this.crud.models.EXPERIMENT, this.experimentId)
     .subscribe(
       (res: Experiment) => {
         console.log(res);
+        
+        // Assign the array of questions
         this.experimentQuestions = res.questions;
+
+        // Use an index of the current question and the amount
+        // of questions in this experiment to control the display
+        // of the questions
+        this.currQuestionIndex = 0;
+        this.questionCount = this.experimentQuestions.length;
+
+        this.assignCurrentlyDisplayedQuestion();
       },
       (err: HttpErrorResponse) => {
         this.errorHandler.handleError(err);
       }
     );
+  }
+
+  /**
+   * Method to assign the current question based on the
+   * array of the experiment's question and the current 
+   * question's index.
+   */
+  assignCurrentlyDisplayedQuestion(){
+
+    this.currentQuestion = this.experimentQuestions[this.currQuestionIndex];
+  }
+
+  /**
+   * Method to change the current question based on the
+   * array of the experiment's question and the current 
+   * question's index. 
+   */
+  changeCurrentlyDisplayedQuestion(){
+
+    // If there is at least one more question left
+    if(this.currQuestionIndex < this.questionCount - 1){
+
+      this.currQuestionIndex++;
+      this.assignCurrentlyDisplayedQuestion();
+    }
+    else{
+
+      // Just finish the experiment
+      this.finishExperiment();
+    }
+  }
+
+  /**
+   * Method to finish the experiment and return to 
+   * the ExperiementRetrieveComponent.
+   */
+  finishExperiment(){
+
+    this.retrieveExperiment(this.experimentId);
+  }
+
+  /**
+   * Method that retrieves an experiment.
+   * @param id the id of the experiment to retrieve
+   */
+  retrieveExperiment(id: number) {
+    this.router.navigate(['experiments/' + id]);
   }
 }
