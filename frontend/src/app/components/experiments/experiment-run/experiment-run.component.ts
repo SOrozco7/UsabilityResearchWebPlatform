f8e646619a09ccbdcfc135ef9f07cb5bd15cda41
ChavaO777 <salvador@orozco.in>
2018-11-04 15:24:09 -0600

    Add the first attempt of a call to retrieve the list of questions of a given experiment in the ExperimentRunComponent.

--- frontend/src/app/components/experiments/experiment-run/experiment-run.component.ts
+++ frontend/src/app/components/experiments/experiment-run/experiment-run.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../../services/crud.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Experiment } from '../../../models/experiment';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ErrorHandlerService } from '../../../services/error-handler.service';
 import { Question } from '../../../models/question';
 
 @Component({
@@ -8,9 +13,28 @@ import { Question } from '../../../models/question';
 })
 export class ExperimentRunComponent implements OnInit {
 
-  constructor() { }
+  experimentQuestions: Question[];
+  experimentId: number;
+
+  constructor(
+    private errorHandler: ErrorHandlerService,
+    private crud: CrudService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
 
+    this.experimentId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+
+    this.crud.retrieve(this.crud.models.EXPERIMENT, this.experimentId)
+    .subscribe(
+      (res: Experiment) => {
+        console.log(res);
+        this.experimentQuestions = res.questions;
+      },
+      (err: HttpErrorResponse) => {
+        this.errorHandler.handleError(err);
+      }
+    );
   }
 }
