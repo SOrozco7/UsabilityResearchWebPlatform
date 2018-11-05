a468ef36bbf6c352288ca95348c768274f6e2f95
Andrés Reynoso Mazoy <andres.reynoso.mazoy@gmail.com>
2018-11-05 12:20:11 -0600

    Fix new component's test

--- frontend/src/app/components/questionnaires/questionnaire-create/questionnaire-create.component.spec.ts
+++ frontend/src/app/components/questionnaires/questionnaire-create/questionnaire-create.component.spec.ts
@@ -1,4 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CrudService } from '../../../services/crud.service';
+import { AuthService } from '../../../services/auth.service';
+import { ErrorHandlerService } from '../../../services/error-handler.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '../../../material.module';
 
 import { QuestionnaireCreateComponent } from './questionnaire-create.component';
 
@@ -8,7 +16,21 @@ describe('QuestionnaireCreateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ QuestionnaireCreateComponent ]
+      declarations: [ QuestionnaireCreateComponent ],
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        BrowserModule,
+        MaterialModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        AuthService,
+        CrudService,
+        ErrorHandlerService,
+        HttpClient,
+        HttpHandler
+      ]
     })
     .compileComponents();
   }));
