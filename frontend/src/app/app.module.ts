c434d94b7b941e22626e392568aaa93bdc3b7f4f
ChavaO777 <salvador@orozco.in>
2018-11-15 11:48:15 -0600

    Add the jQuery dependency for consuming the YouTube Data API for the video uploads.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -28,7 +28,7 @@ import { SignupComponent } from './components/signup/signup.component';
 import { QuestionnaireFillComponent } from './components/questionnaires/questionnaire-fill/questionnaire-fill.component';
 import { QuestionnaireAddToExperimentComponent } from './components/questionnaires/questionnaire-add-to-experiment/questionnaire-add-to-experiment.component'; // tslint:disable-line:max-line-length
 import { ExperimentRunComponent } from './components/experiments/experiment-run/experiment-run.component';
-import { RecordRtcComponent } from './components/record-rtc/record-rtc.component';  // tslint:disable-line:max-line-length
+import { RecordRtcComponent } from './components/record-rtc/record-rtc.component';
 
 @NgModule({
   declarations: [
