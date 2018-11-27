dbe2fdd119f51317320cf7f5aa24d16845cc92f7
ChavaO777 <salvador@orozco.in>
2018-11-27 15:26:37 -0600

    Add a dependency for a test that was failing.

--- frontend/src/app/components/participants/participant-selection/participant-selection.component.spec.ts
+++ frontend/src/app/components/participants/participant-selection/participant-selection.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ScriptService } from '../../../services/script.service';
+import { CrudService } from '../../../services/crud.service';
+import { ErrorHandlerService } from '../../../services/error-handler.service';
 import { ParticipantSelectionComponent } from './participant-selection.component';
+import { AuthService } from '../../../services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ParticipantSelectionComponent', () => {
+
   let component: ParticipantSelectionComponent;
   let fixture: ComponentFixture<ParticipantSelectionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ParticipantSelectionComponent ]
+      declarations: [ ParticipantSelectionComponent ],
+      imports: [RouterTestingModule],
+      providers: [
+        ScriptService,
+        CrudService,
+        ErrorHandlerService,
+        AuthService,
+        HttpClient,
+        HttpHandler
+      ]
     })
     .compileComponents();
   }));
